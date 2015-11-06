aec2fc82a0ddc5b8078b720a0291ee60a5d5dae4
nestor <ngodoy@4geeks.com.ve>
2015-11-06 11:57:49 -0430

    Templates

--- academy/controllers.py
+++ academy/controllers.py
@@ -4,7 +4,10 @@ from openerp import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
-        return "Hello, world"
+        return http.request.render('academy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
+
 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):
